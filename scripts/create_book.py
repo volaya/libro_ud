5d40d2be8178fd8b0e5d5b023a649fac9956feaf
volaya <volayaf@gmail.com>
2020-11-12 17:39:01 +0100

    first version

---
+++ scripts/create_book.py
@@ -0,0 +1,5 @@
+LILYPONDPATH = r"C:\Program Files (x86)\LilyPond\usr\bin" 
+PYTHONPATH = os.path.join("LILYPONDPATH", "python.exe")
+LILYPONBDBOOKPATH = os.path.join(LILYPONDPATH, "lilypond-book.py")
+
+--pdf maqaamat.lytex
