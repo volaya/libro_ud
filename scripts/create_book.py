e6d993fb14f43f17f67d66731790c51f3841814a
volaya <volayaf@gmail.com>
2020-11-15 09:26:55 +0100

    more content

--- scripts/create_book.py
+++ scripts/create_book.py
@@ -1,5 +1,13 @@
+import re
+import os.path
+import shutil
+from subprocess import call
+
 LILYPONDPATH = r"C:\Program Files (x86)\LilyPond\usr\bin" 
 PYTHONPATH = os.path.join("LILYPONDPATH", "python.exe")
 LILYPONBDBOOKPATH = os.path.join(LILYPONDPATH, "lilypond-book.py")
+BOOKPATH = os.path.join(os.dirname(os.path.dirname(__file__)), "libro", "libro.lytex")
+OUTPUTTEXFILE = os.path.join(os.dirname(os.path.dirname(__file__)), "libro", "output", "libro.tex")
 
---pdf maqaamat.lytex
+call([PYTHONPATH, LILYPONBDBOOKPATH, BOOKPATH, "--pdf", "output"])
+call(["pdflatex", OUTPUTTEXFILE])
