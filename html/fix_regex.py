e57d481db2bc18b87868576690a6b77495e90cfb
volaya <volayaf@gmail.com>
2026-08-21 14:12:01 +0200

    web

--- html/fix_regex.py
+++
@@ -1,8 +0,0 @@
-import os
-p='/home/volaya/sources/libro_ud/html/convert.py'
-c=open(p,encoding='utf-8').read()
-c=c.replace("    for f in re.findall(r'\\\\\\\\input\\\\\\\\{([^}]+)\\\}', c):","    for f in re.findall(r'\\\\\\\\\\\\\\\\input\\\\\\\\\\\\\\\\{([^}]+)\\\\\\\\\\\\\\\\\}', c):")
-c=c.replace("            c = c.replace(r'\\\\\\\\input{' + f + '}', read_inputs(p, os.path.dirname(p)))","            c = c.replace(r'\\\\\\\\\\\\\\\\input{' + f + '}', read_inputs(p, os.path.dirname(p)))")
-c=c.replace("            c = c.replace(r'\\\\\\\\input{' + f + '}', '')","            c = c.replace(r'\\\\\\\\\\\\\\\\input{' + f + '}', '')")
-open(p,'w',encoding='utf-8').write(c)
-print('fixed')
